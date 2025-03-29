aa84fc19ce1b86a8de596ded16b4dd27a4593ecb
sondernextdoor <63323642+sondernextdoor@users.noreply.github.com>
2025-03-28 22:16:35 -0400

    Create windows_kernel_framework.h

---
+++ windows_kernel_framework.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <ntddk.h>
+#include "../src/common/utilities.h"
+#include "../src/patchguard/pg_context.h"
+#include "../src/hvci/hvci_policy.h"
+#include "../src/vbs/secure_memory.h"
+#include "../src/tpm_pluton/tpm_integration.h"
